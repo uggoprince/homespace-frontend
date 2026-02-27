f143f6bea8c123a1c5ef7c4d8d17ccc24183b73b
Ugo Anayo <uggoprince@gmail.com>
2026-02-28 00:11:09 +0100

    fix build errors

--- src/Utils/cn.ts
+++ src/Utils/cn.ts
@@ -1,11 +1,6 @@
-import { clsx } from 'clsx';
+import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-/**
- * Utility function to merge Tailwind CSS classes with clsx
- * @param {...any} inputs - Class names to merge
- * @returns {string} Merged class names
- */
-export function cn(...inputs) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
